4e644f2073b02cbec42cbc905ba0e1e62586b1a5
micaelagoffe <micaela.goffe@gmail.com>
2021-06-11 14:56:22 +0200

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
+
+import './styles/Header.css';
+
 export function Header() {
     return (
       <>
         <header>
             <p>Välkommen på</p>
-            <h1>Zoo</h1>
+            <Link to="/" className="link"><h1><span>Z</span><span>o</span><span>o</span></h1></Link>
         </header>
       </>
     );
